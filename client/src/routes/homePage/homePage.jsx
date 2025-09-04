f960992ecf8f72b5007451716b6925d6ba52bc70
aman7661 <agcoding123@gmail.com>
2025-09-05 01:14:44 +0530

    first commit

---
+++ client/src/routes/homePage/homePage.jsx
@@ -0,0 +1,52 @@
+import { useContext } from "react";
+import SearchBar from "../../components/searchBar/SearchBar";
+import "./homePage.scss";
+import { AuthContext } from "../../context/AuthContext";
+
+function HomePage() {
+
+  const {currentUser} = useContext(AuthContext)
+
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
+            explicabo suscipit cum eius, iure est nul
+            <br/>
+            <br/>
+            Test Credentials 1:
+            Username: john20,
+            Pass: 123456
+            <br/>
+            Test Credentials 2:
+            Username: john1,
+            Pass: 123456
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Award Gained</h2>
+            </div>
+            <div className="box">
+              <h1>2000+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
